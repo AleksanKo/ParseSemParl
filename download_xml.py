5959f83bb13d183cbbb16c4c6996044b7d24691e
AleksanKo <aleksandra.konovalova@helsinki.fi>
2020-02-27 09:54:10 +0200

    changes in script

--- download_xml.py
+++ download_xml.py
@@ -5,7 +5,6 @@ import re
 import requests
 from bs4 import BeautifulSoup, Tag, NavigableString
 import pandas as pd
-from pprint import pprint
 
 jsons = []
 
@@ -18,7 +17,6 @@ def download_jsons(mode):
         json_data = json.loads(response.content)
         jsons.append(json_data)
     elif mode == 'folder':
-        text_of_the_document = ''
         link = 'C:/Users/konovale/Parsing/data_txt/'
         onlyfiles = [f for f in listdir(link) if isfile(join(link, f))]
         id_search = re.compile('PTK\s\d{1,4}\/\d{4}\svp')
@@ -34,77 +32,64 @@ def download_jsons(mode):
         ids = list(set(ids))
         for id_doc in ids:
             response = requests.get('https://avoindata.eduskunta.fi/api/v1/tables/VaskiData/rows?perPage=10&page=0&columnName=Eduskuntatunnus&columnValue={}'.format(id_doc))
-            #responses.append(response)
             json_data = json.loads(response.content)
             jsons.append(json_data)
         print('{} JSONs gathered'.format(len(jsons)))
     return jsons
 
-json1 = download_jsons('folder')
-alll_adata = []
-print(json1)
-print(len(json1))
-for xml in json1:
-    for i in xml['rowData']:
-        l2 = i[1]
-        soup = BeautifulSoup(l2)
-        #if 'Alkoholijuomien anniskelu sallitaan' in t:
-        #    print('wow this string is in xml')
-        print(soup.prettify())
-        items = soup.find_all("vsk:puheenvuorotoimenpide")
-        #items = soup.findAll("sis:kappalekooste")
-        print(len(items))
-        day = soup.find("met1:nimeketeksti")
-        print(day)
-        date = re.search(r'\d{1,2}\.\d{1,2}.\d{4}',day.contents[0]).group()
+def parsing_xml():
+    json1 = download_jsons('folder')
+    overall_data = []
 
-        all_dates = []
-        all_times = []
-        all_persons = []
-        all_parties = []
-        all_texts = []
+    for xml in json1:
+        for i in xml['rowData']:
+            l2 = i[1]
+            soup = BeautifulSoup(l2)
+            #finding all speeches by tag
+            items = soup.find_all("vsk:puheenvuorotoimenpide")
 
-        for i in range(0,len(items)):
-            data_list = []
-            print('Now the list is empty')
-            for j in items[i].descendants:
-                print('For every descendant ', len(data_list))
-                if isinstance(j, Tag):
-                    for k in j.contents:
-                        print('For every content', len(data_list))
-                        if isinstance(k, NavigableString):
-                            print('Now smth is appended')
-                            data_list.append(k)
-                            print('After append: ',len(data_list))
-                        print('After if:', len(data_list))
-                    print('After for contents',len(data_list))
-                print('After Tag: ', len(data_list))
-            print('After going desc', len(data_list))
-            #print('After RANGE', len(data_list))
-            alll_adata.append(data_list)
-            #print(data_list)
-        print(len(alll_adata))
-for data_list in alll_adata:
-    all_dates.append(date)
-    all_times.append(data_list[0])
-    if 'ministeri' in data_list[1]:
-        all_persons.append(data_list[2] + ' ' + data_list[3])
-        all_parties.append('minister')
-    else:
-        all_persons.append(data_list[1] + ' ' + data_list[2])
-        all_parties.append(data_list[3])
-    if 'puheenvuoro)' in data_list[4]:
-        all_texts.append(' '.join(data_list[5:]))
-    else:
-        all_texts.append(' '.join(data_list[4:]))
-print('persons',len(all_persons))
-print('dates',len(all_dates))
-print(len(all_dates)==len(all_persons))
-all_data = {'Date': all_dates,'Time': all_times, 'Speaker': all_persons, 'Party': all_parties,'Speech': all_texts}
-print(len(all_data))
-df = pd.DataFrame(all_data)
-print(df)
-df.to_csv('C:/Users/konovale/Parsing/speeches_from_xml.csv', encoding='utf-8', sep='\t', mode='a', header=False)
-# print(len(data_list))PTK 70/2019 vp
+            all_dates = []
+            all_times = []
+            all_persons = []
+            all_parties = []
+            all_texts = []
 
+            for i in range(0,len(items)):
+                data_list = []
+                #searching for attribute containing time of the beginning of the speech in format e.g. '2017-09-19T14:15:46'
+                day = items[i]['vsk1:puheenvuoroaloitushetki']
+                data_list.append(day)
 
+                #gathering all necessary contents
+                for j in items[i].descendants:
+                    if isinstance(j, Tag):
+                        for k in j.contents:
+                            if isinstance(k, NavigableString):
+                                data_list.append(k)
+                overall_data.append(data_list)
+
+    for data_list in overall_data:
+
+        all_dates.append(data_list[0][0:10]) #because time is stored in format "2017-09-19T14:15:46"
+        all_times.append(data_list[0][11:])
+
+        #ministers don't have parties in data, so instead of the abbreviation 'minister' is appended
+        if 'ministeri' in data_list[2]:
+            all_persons.append(data_list[3] + ' ' + data_list[4])
+            all_parties.append('minister')
+        else:
+            all_persons.append(data_list[2] + ' ' + data_list[3])
+            all_parties.append(data_list[4])
+        #we don't need the data about whether the speech is a response or some other type of speech, so we don't append it
+        if 'puheenvuoro)' in data_list[5]:
+            all_texts.append(' '.join(data_list[6:]))
+        else:
+            all_texts.append(' '.join(data_list[5:]))
+
+    all_data = {'Date': all_dates,'Time': all_times, 'Speaker': all_persons, 'Party': all_parties,'Speech': all_texts}
+    df = pd.DataFrame(all_data)
+    return df
+
+final_df = parsing_xml()
+path = input('Enter a path to the CSV file where to save the data: ', )
+final_df.to_csv(path, encoding='utf-8', sep='\t', mode='a', header=False)
